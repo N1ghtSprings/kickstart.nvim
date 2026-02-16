310c64b721a4a4f79500066ca1523d735c630f1a
Filip Pesic <pesic.filip99@gmail.com>
2026-02-17 00:43:35 +0100

    update config

--- init.lua
+++ init.lua
@@ -8,6 +8,13 @@ vim.keymap.set('n', '<C-s>', ':w<CR>', { silent = true, desc = 'Make Ctrl + s do
 
 vim.opt.fillchars:append { eob = ' ' }
 
+vim.opt.hlsearch = false
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
 vim.g.netrw_banner = 0
 vim.g.netrw_list_hide = [[\(^\|\s\s\)\zs\.\S\+]]
 vim.g.netrw_hide = 1
@@ -25,30 +32,22 @@ vim.keymap.set('n', '<leader>ls', function()
   vim.cmd 'startinsert'
 end, { desc = 'Open terminal in vertical split' })
 
--- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.keymap.set('n', '<leader>*', function()
+  vim.fn.setreg('/', '\\<' .. vim.fn.expand '<cword>' .. '\\>')
+  vim.o.hlsearch = false
+  vim.cmd 'redraw'
+end, { desc = 'Search word under cursor and highlight' })
+
 vim.g.have_nerd_font = true
 
--- [[ Setting options ]]
--- See `:help vim.o`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
-
--- Make line numbers default
 vim.o.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
+
 vim.o.relativenumber = true
 
--- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
 
--- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
 vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
 vim.g.clipboard = {
   name = 'win32yank-wsl',
@@ -62,7 +61,7 @@ vim.g.clipboard = {
   },
   cache_enabled = 0,
 }
--- Enable break indent
+
 vim.o.breakindent = true
 
 -- Save undo history
@@ -126,7 +125,7 @@ vim.diagnostic.config {
   underline = { severity = vim.diagnostic.severity.ERROR },
 
   -- Can switch between these as you prefer
-  virtual_text = true, -- Text shows up at the end of the line
+  virtual_text = false, -- Text shows up at the end of the line
   virtual_lines = false, -- Teest shows up underneath the line, with virtual lines
 
   -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
@@ -201,14 +200,6 @@ rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  {
-    'lmgraf/wsl-clipboard.nvim',
-    opts = {
-      mode = 'sync',
-      on_focus = false,
-    },
-  },
-
   -- NOTE: Plugins can be added via a link or github org/name. To run setup automatically, use `opts = {}`
   { 'NMAC427/guess-indent.nvim', opts = {} },
 
@@ -742,7 +733,7 @@ require('lazy').setup({
       fuzzy = { implementation = 'lua' },
 
       -- Shows a signature help window while you type arguments for a function
-      signature = { enabled = true },
+      signature = { enabled = false },
     },
   },
 
@@ -789,6 +780,10 @@ require('lazy').setup({
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
+      require('mini.move').setup()
+
+      require('mini.pairs').setup()
+
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
@@ -811,6 +806,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     config = function()
+      ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup {
         ensure_installed = {
           'bash',
@@ -848,7 +844,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  require 'kickstart.plugins.autopairs',
+  -- require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
